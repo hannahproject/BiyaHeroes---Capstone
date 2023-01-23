a1c8a7692525ff71fec5319c806677d1d9debc6f
hannah <liwanagportfolio@gmail.com>
2023-01-23 14:33:22 -0800

    profile popup validation

--- js/admin-edit-profile.js
+++ js/admin-edit-profile.js
@@ -14,6 +14,8 @@ const account_popup = document.getElementById('account-popup');
 const password_popup = document.getElementById('password-popup');
 const mask = document.getElementById('page-mask');
 
+const btn = document.querySelectorAll('#btn');
+
 //FILEPOND SETUP
 FilePond.registerPlugin(
     FilePondPluginImagePreview,
@@ -79,6 +81,21 @@ FilePond.registerPlugin(
   function ClosePopup() {
     mask.style.visibility = 'hidden';
 
+    const inputs = document.getElementsByClassName('form-control');
+
+    for(var input of inputs) {
+      const err = input.classList.contains('error');
+      const success = input.classList.contains('success');
+
+      if(err === true) {
+        input.classList.remove('error');
+      }
+
+      if(success === true) {
+        input.classList.remove('success');
+      }
+    }
+
     for(var popup of popups) {
       popup.style.display = 'none';
     }
@@ -117,7 +134,86 @@ FilePond.registerPlugin(
     }
   }
 
+  function FormValidation() {
+
+    const firstname = document.getElementById('edit-firstname');
+    const lastname = document.getElementById('edit-lastname');
+    const address = document.getElementById('edit-address');
+    const birthday = document.getElementById('edit-birthday');
+    const phone = document.getElementById('edit-phone');
+    const email = document.getElementById('edit-email');
+    const current_pass = document.getElementById('current-pass');
+    const new_pass = document.getElementById('new-pass');
+    const retype_pass = document.getElementById('retype-pass');
+
+    if(firstname.value === '') {
+      Err(firstname);
+    } else {
+      Success(firstname);
+    }
+
+    if(lastname.value === '') {
+      Err(lastname);
+    } else {
+      Success(lastname);
+    }
+
+    if(address.value === '') {
+      Err(address);
+    } else {
+      Success(address);
+    }
+
+    if(birthday.value === '') {
+      Err(birthday);
+    } else {
+      Success(birthday);
+    }
+
+    if(phone.value === '') {
+      Err(phone);
+    } else {
+      Success(phone);
+    }
+
+    if(email.value === '') {
+      Err(email);
+    } else {
+      Success(email);
+    }
+
+    if(current_pass.value === '') {
+      Err(current_pass);
+    } else {
+      Success(current_pass);
+    }
+
+    if(new_pass.value === '') {
+      Err(new_pass);
+    } else {
+      Success(new_pass);
+    }
+
+    if(retype_pass.value === '') {
+      Err(retype_pass);
+    } else {
+      Success(retype_pass);
+    }
+  }
+
+  function Success(input) {
+    input.className = 'form-control success';
+  }
+
+  function Err(input) {
+    input.className = 'form-control error';  
+  }
+
   edit_profile.addEventListener('click', EditProfile);
   edit_personal.addEventListener('click', EditPersonal);
   edit_account.addEventListener('click', EditAccount);
   change_pass.addEventListener('click', ChangePassword);
+
+  for(var b of btn) {
+    b.addEventListener('click', FormValidation);
+  }
